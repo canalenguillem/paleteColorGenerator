46b633876850f751128d72aa0aabb8215e25f63a
enguillem <enguillem@gmail.com>
2024-10-11 08:49:17 +0200

    configurando flask

---
+++ flask/app.py
@@ -0,0 +1,15 @@
+from flask import Flask,render_template,request
+
+app=Flask(__name__,
+          template_folder='templates'
+          )
+
+
+
+@app.route("/")
+def index():
+    return "hello from flasksss"
+
+
+if __name__=="__main__":
+    app.run(debug=True)
